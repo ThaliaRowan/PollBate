a7517f99935b2c5b1da15b275f1021f611f880b8
ThaliaRowan <thalia.rowan@gmail.com>
2021-02-28 22:07:55 -0400

    finished Header for now

--- pollbate/src/components/Header.js
+++ pollbate/src/components/Header.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "../Header.css";
+import { Button } from "@material-ui/core";
+
 
 function Header(){
     return(
         <div className="Header">
             <div className="Title">
                 <h2>PollBate</h2>
+            </div>
+            <div className="listItems">
+                <ul>
+                    <li>
+                        <Button className="createPoll" >Create Poll</Button>
+                    </li>
+                    <li>
+                        <p>Sign Up</p>
+                    </li>
+                </ul>
             </div>
         </div>
     );
